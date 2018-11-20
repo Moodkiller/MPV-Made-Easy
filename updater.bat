e0cabd7f12e8bf6087c783d023d10a3fc11b71de
Moodkiller <appleipodsrock@gmail.com>
2018-11-20 17:51:49 +1300

    Added more config options
    Included higher video output quaility, changed volume levels and added subtitle functions.

--- updater.bat
+++ updater.bat
@@ -6,9 +6,10 @@ if exist "%~dp0\installer\updater.ps1" (
 ) else (
     set updater_script="%~dp0\updater.ps1"
 )
-powershell -noprofile -nologo -noexit -executionpolicy bypass -File %updater_script%
+powershell -noprofile -nologo -executionpolicy bypass -File %updater_script%
 
 :: After update, updater.ps1 should not in same folder as mpv.exe
 if exist "%~dp0\installer\updater.ps1" if exist "%~dp0\updater.ps1" (
     del "%~dp0\updater.ps1"
 )
+@pause
