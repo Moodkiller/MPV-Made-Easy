17da8bb2ee7d6caa4eef2a06a655daa5849aec35
Moodkiller <appleipodsrock@gmail.com>
2018-12-15 17:09:22 +1300

    NOW included updater.ps1 so that updater.bat works

--- updater.bat
+++ updater.bat
@@ -8,8 +8,4 @@ if exist "%~dp0\installer\updater.ps1" (
 )
 powershell -noprofile -nologo -executionpolicy bypass -File %updater_script%
 
-:: After update, updater.ps1 should not in same folder as mpv.exe
-if exist "%~dp0\installer\updater.ps1" if exist "%~dp0\updater.ps1" (
-    del "%~dp0\updater.ps1"
-)
 @pause
